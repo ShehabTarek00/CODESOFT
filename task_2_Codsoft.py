b1ded71c79c721a01aaf3b9102ca92810e5a7609
ShehabTarek00 <144713202+ShehabTarek00@users.noreply.github.com>
2023-09-12 11:33:23 +0300

    Add files via upload

---
+++ task_2_Codsoft.py
@@ -0,0 +1,28 @@
+def sum(n,m):
+    return (n+m)
+def diff(n,m):
+    return (n-m)
+def product(n,m):
+    return (n*m)
+def division(n,m):
+    return (n/m)
+turn=1
+while (turn):
+    x=int(input('Enter the first number : '))
+    y=int(input('Enter the second number : '))
+    o=input('Enter the operation you want to be calculated : ')
+    if(o == '+' or o == 'sum' or o == 'add' or o=='plus'):
+        print('the Result = ',sum(x,y))
+    elif(o == '-' or o == 'difference' or o == 'minus' or o=='subtract'):
+        print('the Result = ',diff(x,y))
+    elif(o == '*' or o == 'x' or o == 'product' or o == 'times' or o=='multiply'):
+        print('the Result = ',product(x,y))
+    elif( o == '/' or o == 'division' or o=='divide'):
+        div=round(division(x,y),4)
+        print('the Result = ',div)
+    print('Do you want another calculation ? ')
+    ans=input('yes or no ? ')
+    if(ans == 'yes'):
+        turn=1
+    elif(ans == 'no'):
+        turn=0
